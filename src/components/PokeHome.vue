90a8c9f917589243a0a883819b7a27ddcea1f37d
Jalles <jallessantos12@gmail.com>
2023-02-10 01:19:29 -0300

    feat: create list card and some alters in css

--- src/components/PokeHome.vue
+++ src/components/PokeHome.vue
@@ -1,8 +1,9 @@
 <template>
-  <PokeHeader v-on:search-pokemon="searchPokemon" />
+  <PokeHeader v-on:search-pokemon="searchPokemonSpecies" />
   <main>
     <div class="container">
       <div class="nothing-message" v-if="pokemons.length <= 0">Nada por enquanto...</div>
+      <PokeList v-if="pokemons.length" :pokemons="pokemons" />
     </div>
   </main>
 </template>
@@ -10,25 +11,103 @@
 <script>
 import instance from "@/server/axios";
 import PokeHeader from "./PokeHeader.vue";
+import PokeList from "./PokeList.vue";
 export default {
   components: {
     PokeHeader,
+    PokeList,
   },
   data() {
     return {
+      pokemonsNames: [],
       pokemons: [],
     };
   },
   methods: {
-    async searchPokemon({ search }) {
+    async searchPokemonSpecies({ search }) {
+      this.pokemons = [];
+      this.pokemonsNames = [];
+      if (search && typeof search === "string") {
+        try {
+          const specie = await instance.get(`/pokemon-species/${search.toLowerCase().trim()}`);
+          const { data: specieData } = specie;
+          const chain = await this.searchPokemonEvoChain(specieData.evolution_chain.url);
+          const { data: chainData } = chain;
+          this.setPokemonsToSearch(chainData.chain);
+        } catch (error) {
+          console.log(error);
+          return;
+        }
+      }
+    },
+    async searchPokemonEvoChain(url) {
       try {
-        const response = await instance.get(`/pokemon/${search.toLowerCase()}`);
-        console.log(response);
+        const response = await instance.get(url);
         return response;
       } catch (error) {
         console.log(error);
-        return;
       }
+    },
+    chainLoop(evo) {
+      this.pokemonsNames.push({
+        name: evo.species.name,
+      });
+    },
+    setPokemonsToSearch(chain) {
+      this.chainLoop(chain);
+      chain.evolves_to.forEach((evo, index, arr) => {
+        this.chainLoop(evo);
+        if (arr.length) {
+          evo.evolves_to.forEach((evo, index, arr) => {
+            if (arr.length) {
+              this.chainLoop(evo);
+
+              evo.evolves_to.forEach((evo, index, arr) => {
+                if (arr.length) {
+                  this.chainLoop(evo);
+
+                  evo.evolves_to.forEach((evo, index, arr) => {
+                    if (arr.length) {
+                      this.chainLoop(evo);
+                      console.log(evo.evo.evolves_to.length);
+                    }
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+    },
+    async searchPokemonsStats(name) {
+      try {
+        const response = await instance.get(`/pokemon/${name.toLowerCase().trim()}`);
+        const { data } = response;
+
+        const pokeInfo = {
+          image: data.sprites.other.dream_world.front_default,
+          order: data.order,
+          name: data.name,
+        };
+
+        data.stats.forEach(({ base_stat, stat }) => {
+          pokeInfo[`${stat.name}`] = base_stat;
+        });
+
+        this.pokemons.push(pokeInfo);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  watch: {
+    pokemonsNames: {
+      handler(val) {
+        Object.values(val).forEach(({ name }) => {
+          this.searchPokemonsStats(name);
+        });
+      },
+      deep: true,
     },
   },
 };
@@ -36,7 +115,7 @@ export default {
 
 <style>
 main {
-  margin-top: 20px;
+  margin-top: 120px;
 }
 .nothing-message {
   width: 90%;
